05bd67437d463785b2787ffe5b65ca9af43d1412
Lich <lichnguyenhong.it@gmail.com>
2023-03-23 21:32:04 +0700

    Add db to project

---
+++ mongoDB_init.py
@@ -0,0 +1,25 @@
+import pymongo
+from dotenv import load_dotenv
+import os
+from urllib.parse import quote_plus
+load_dotenv()
+
+mongodb_password = os.environ['mongodb_password']
+mongodb_password = quote_plus(mongodb_password)
+
+connect_string = f'mongodb+srv://hdttuan:{mongodb_password}@cluster0.h09da4d.mongodb.net/?retryWrites=true&w=majority'
+
+crawlClient = pymongo.MongoClient(connect_string)
+crawlClient = crawlClient['TRACKINGINVESTMENT_CRAWL']
+
+
+
+
+
+
+
+
+
+
+
+
