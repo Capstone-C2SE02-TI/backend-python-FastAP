16f28aa3acb336a75bc486d1f772d99d1f056e95
Lich <lichnguyenhong.it@gmail.com>
2023-05-05 22:08:34 +0700

    Update requirements.txt

--- test.py
+++ test.py
@@ -1,20 +1,20 @@
-I would like to send training daily report.
-1. Today's tasks
-* Improving Deep Neural Networks: Hyperparameter Tuning, Regularization and Optimization:
-    - W2 : Optimization Algorithms
-    - W3 : Hyperparameter Tuning, Batch Normalization and Programming Frameworks
+# I would like to send training daily report.
+# 1. Today's tasks
+# * Improving Deep Neural Networks: Hyperparameter Tuning, Regularization and Optimization:
+#     - W2 : Optimization Algorithms
+#     - W3 : Hyperparameter Tuning, Batch Normalization and Programming Frameworks
 
-* Research face-embedding source code:
-    - Research about face identification
-        ~ research about Arcface
+# * Research face-embedding source code:
+#     - Research about face identification
+#         ~ research about Arcface
  
 
-2. Tomorrow's tasks:
-* Improving Deep Neural Networks: Hyperparameter Tuning, Regularization and Optimization:
-    - W3 : Hyperparameter Tuning, Batch Normalization and Programming Frameworks
+# 2. Tomorrow's tasks:
+# * Improving Deep Neural Networks: Hyperparameter Tuning, Regularization and Optimization:
+#     - W3 : Hyperparameter Tuning, Batch Normalization and Programming Frameworks
 
-* Research face-embedding source code:
-    - Research about face identification
-        ~ research about Arcface
+# * Research face-embedding source code:
+#     - Research about face identification
+#         ~ research about Arcface
 
-3.Issues or Comment: None
+# 3.Issues or Comment: None
